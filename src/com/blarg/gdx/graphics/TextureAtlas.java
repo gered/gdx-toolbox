cc17afdd8e96b25cc2dd8bc2af6188f9c0a1fc95
gered <gered@blarg.ca>
2013-07-03 18:14:27 -0400

    add TextureAtlas base class

---
+++ src/com/blarg/gdx/graphics/TextureAtlas.java
@@ -0,0 +1,28 @@
+package com.blarg.gdx.graphics;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
+
+public abstract class TextureAtlas {
+	public static final float TEXCOORD_EDGE_BLEED_OFFSET = 0.02f;
+
+	public final Texture texture;
+
+	protected final float edgeCoordOffset;
+	protected Array<TextureRegion> tiles = new Array<TextureRegion>(TextureRegion.class);
+
+	public TextureAtlas(Texture texture, float edgeCoordOffset) {
+		this.texture = texture;
+		this.edgeCoordOffset = edgeCoordOffset;
+	}
+
+	public TextureRegion get(int index) {
+		return tiles.items[index];
+	}
+
+	public int count() {
+		return tiles.size;
+	}
+}
