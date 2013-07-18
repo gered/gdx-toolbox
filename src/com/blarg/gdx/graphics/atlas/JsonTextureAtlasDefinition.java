54823cd80411e421e5dcf028b2d2fd39440ba118
gered <gered@blarg.ca>
2013-07-18 09:02:05 -0400

    add json config POJOs

---
+++ src/com/blarg/gdx/graphics/atlas/JsonTextureAtlasDefinition.java
@@ -0,0 +1,8 @@
+package com.blarg.gdx.graphics.atlas;
+
+import java.util.ArrayList;
+
+public class JsonTextureAtlasDefinition {
+	public String texture;
+	public ArrayList<JsonTextureAtlasTile> tiles;
+}
---
+++ src/com/blarg/gdx/graphics/atlas/JsonTextureAtlasTile.java
@@ -0,0 +1,14 @@
+package com.blarg.gdx.graphics.atlas;
+
+public class JsonTextureAtlasTile {
+	public boolean autogrid;
+	public int x;
+	public int y;
+	public int width;
+	public int height;
+	public int numTilesX;
+	public int numTilesY;
+	public int tileWidth;
+	public int tileHeight;
+	public int border;
+}
