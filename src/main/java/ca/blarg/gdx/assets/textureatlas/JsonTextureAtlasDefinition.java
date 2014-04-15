d8cb3f2a4aa49dffda9a763f4997f27a35f67296
gered <gered@blarg.ca>
2014-04-15 16:50:50 -0400

    add materialMapping property

--- src/main/java/ca/blarg/gdx/assets/textureatlas/JsonTextureAtlasDefinition.java
+++ src/main/java/ca/blarg/gdx/assets/textureatlas/JsonTextureAtlasDefinition.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class JsonTextureAtlasDefinition {
 	public String texture;
+	public String materialMapping;
 	public ArrayList<JsonTextureAtlasTile> tiles;
 	public ArrayList<JsonTextureAtlasAnimation> animations;
 }
