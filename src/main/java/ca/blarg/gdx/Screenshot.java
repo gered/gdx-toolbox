61cdeea7bb0cfe9fbbbe6cf26bb3751bd2fc89e1
gered <gered@blarg.ca>
2014-04-22 18:08:34 -0400

    add Screenshot class (for taking screenshots programmatically, duh)

---
+++ src/main/java/ca/blarg/gdx/Screenshot.java
@@ -0,0 +1,71 @@
+package ca.blarg.gdx;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
+
+import java.nio.ByteBuffer;
+
+public class Screenshot {
+	static int counter = 1;
+
+	public static void take(String createInPath) {
+		String prefix;
+		if (Strings.isNullOrEmpty(createInPath))
+			prefix = "screenshot";
+		else if (createInPath.endsWith("/"))
+			prefix = createInPath + "screenshot";
+		else
+			prefix = createInPath + "/screenshot";
+
+		try {
+			FileHandle file;
+			do {
+				file = new FileHandle(prefix + counter + ".png");
+				counter++;
+			} while (file.exists());
+			Pixmap pixmap = getFrameBufferPixmap(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+			PixmapIO.writePNG(file, pixmap);
+			pixmap.dispose();
+			Gdx.app.log("Screenshot", String.format("Saved screenshot to %s", file.file().getAbsolutePath()));
+		} catch (Exception e) {
+		}
+	}
+
+	public static void take() {
+		take(null);
+	}
+
+	// Why this method instead of libgdx's own ScreenUtils.getFrameBufferPixmap() ?
+	// This one converts to a 24bit (RGB) PNG which seems to look right a lot more consistently.
+	// ScreenUtils.getFrameBufferPixmap() creates 32bit (RGBA) Pixmaps which tend to look a bit weird when
+	// the image contains some partially transparent pixels (we would rather assume a black background for any
+	// kind of partially transparent pixels for screenshot purposes).
+
+	private static Pixmap getFrameBufferPixmap(int x, int y, int w, int h, boolean flipY) {
+		Gdx.gl.glPixelStorei(GL10.GL_PACK_ALIGNMENT, 1);
+
+		final Pixmap pixmap = new Pixmap(w, h, Pixmap.Format.RGB888);
+		ByteBuffer pixels = pixmap.getPixels();
+		Gdx.gl.glReadPixels(x, y, w, h, GL10.GL_RGB, GL10.GL_UNSIGNED_BYTE, pixels);
+
+		final int numBytes = w * h * 3;
+		byte[] lines = new byte[numBytes];
+		if (flipY) {
+			final int numBytesPerLine = w * 3;
+			for (int i = 0; i < h; i++) {
+				pixels.position((h - i - 1) * numBytesPerLine);
+				pixels.get(lines, i * numBytesPerLine, numBytesPerLine);
+			}
+			pixels.clear();
+			pixels.put(lines);
+		} else {
+			pixels.clear();
+			pixels.get(lines);
+		}
+
+		return pixmap;
+	}
+}
